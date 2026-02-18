8b858f613110f8a9f3a8d151b9eea453e1468e95
Michael Ganesh <34307472+MichyAsg@users.noreply.github.com>
2026-02-18 12:17:01 +0100

    added blazor wasm and fixed api not running

---
+++ AZ204Notes.Web/Program.cs
@@ -0,0 +1,15 @@
+using AZ204Notes.Web;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// Add MudBlazor
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
